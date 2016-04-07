6f789e5b3c252ace5a031dfeeb2ed0a4a47bec04
zyw0228 <zou_ya_wen@163.com>
2016-04-07 13:31:51 +0800

    task_35 finished

--- spring_task/stage_2/task_35/task_35_zyw.js
+++ spring_task/stage_2/task_35/task_35_zyw.js
@@ -1,18 +1,31 @@
 var ulNumNode = document.querySelector(".ul-num");
 var pos = {
-	x:1,
-	y:1,
-	dir:0
+	x:4,
+	y:4,
+	dir:0  //0:上,1:右，2：下，3：左
 }; //小方块状态参数
 var funQueue = {};//动画队列
 
 (function() {
 	var btnNode = document.querySelector(".execute");
-	var textareaNode = document.querySelector(".input-text")
+	var clearNode = document.querySelector(".reset");
+	var textareaNode = document.querySelector(".input-text");
+	addHander(clearNode, "click", resetText);
 	addHander(btnNode, "click", play);
 	addHander(textareaNode, "keydown", addNum);//左边编号数量同步
 	addHander(textareaNode, "scroll", syncNum);//左边编号同步滚动
 })();
+
+//清除textarea数据
+function resetText() {
+	document.querySelector(".input-text").value = '';
+	var liNumNode = document.querySelectorAll(".ul-num li");
+	funQueue ={};
+	liNumNode[0].style.backgroundColor = '#ccc';
+	for (var i = liNumNode.length - 1; i > 0; i--) {
+		ulNumNode.removeChild(liNumNode[i]);
+	}
+}
 
 //左边编号数量同步
 function addNum(event) {
@@ -53,6 +66,7 @@ function getValue() {
 	return textArr;
 }
 
+//检测是否是数字
 function checkNum(data) {
 	 return /^[1-9]+[0-9]*]*$/.test(data);
 }
@@ -75,11 +89,18 @@ function countO(o) {
 
 //执行动画
 function play() {
+	command();
 	var count = countO(funQueue);
-	for (var i = count - 1; i >= 0; i--) {
-		var m = i+1;
-		eval(funQueue[m].fun+"("+funQueue[m].num+")");
-		animate();
+	var m = count;
+	if (count) {
+		var timer = setInterval(function () {
+			eval(funQueue[m].fun+"("+funQueue[m].num+")");
+			animate();
+			m--;
+			if (m<1) {
+				clearTimeout(timer);
+			}
+		}, 1000)
 	}
 }
 
@@ -144,71 +165,6 @@ function command() {
 			liNumNode[lengthT].style.borderRadius = '0px';
 			liNumNode[lengthT].style.backgroundColor = '#ccc';
 		}
-		// switch (rowValue) {
-		// 	case 'go':
-		// 	case 'GO': {
-		// 		go(1);
-		// 		break;
-		// 	}
-		// 	case 'tun lef':
-		// 	case 'TUN LEF': {
-		// 	tunLef();
-		// 	break;
-		// 	}
-		// 	case 'tun rig':
-		// 	case 'TUN RIG': {
-		// 	tunRig();
-		// 	break;
-		// 	}
-		// 	case 'tun bac':
-		// 	case 'TUN BAC': {
-		// 	tunBac();
-		// 	break;
-		// 	}
-		// 	case 'tra lef':
-		// 	case 'TRA LEF': {
-		// 	traLef(1);
-		// 	break;
-		// 	}
-		// 	case 'tra top':
-		// 	case 'TRA TOP': {
-		// 	traTop(1);
-		// 	break;
-		// 	}
-		// 	case 'tra rig':
-		// 	case 'TRA RIG': {
-		// 	traRig(1);
-		// 	break;
-		// 	}
-		// 	case 'tra bot':
-		// 	case 'TRA BOT': {
-		// 	traBot(1);
-		// 	break;
-		// 	}
-		// 	case 'mov lef':
-		// 	case 'MOV LEF': {
-		// 	movLef(1);
-		// 	break;
-		// 	}
-		// 	case 'mov top':
-		// 	case 'MOV TOP': {
-		// 	movTop(1);
-		// 	break;
-		// 	}
-		// 	case 'mov rig':
-		// 	case 'MOV RIG': {
-		// 	movRig(1);
-		// 	break;
-		// 	}
-		// 	case 'mov bot':
-		// 	case 'MOV BOT': {
-		// 	movBot(1);
-		// 	break;
-		// 	}
-
-		// }
-		// animate();
-		// funQueue.lengthT
 	}
 }
 
@@ -225,17 +181,37 @@ function tunBac(n) {
 }
 
 function go(n) {
-	if ((Math.abs(positive(pos.dir)%4) == 0)&&(pos.y>n-1)) {
-		pos.y = pos.y-n;
+	if (Math.abs(positive(pos.dir)%4) == 0) {
+		if (pos.y>n-1) {
+			pos.y = pos.y-n;
+		}
+		else {
+			pos.y = 0;
+		}
 	}
-	else if((Math.abs(positive(pos.dir)%4) == 1)&&(pos.x<10-n)) {
-		pos.x = pos.x+n;
+	else if(Math.abs(positive(pos.dir)%4) == 1) {
+		if (pos.x<10-n) {
+			pos.x = pos.x+n;
+		}
+		else {
+			pos.x = 9;
+		}
 	}
-	else if((Math.abs(positive(pos.dir)%4) == 2)&&(pos.y<10-n)) {
-		pos.y = pos.y+n;
+	else if(Math.abs(positive(pos.dir)%4) == 2) {
+		if (pos.y<10-n) {
+			pos.y = pos.y+n;
+		}
+		else {
+			pos.y = 9;
+		}
 	}
-	else if((Math.abs(positive(pos.dir)%4) == 3)&&(pos.x>n-1)) {
-		pos.x = pos.x-n;
+	else if(Math.abs(positive(pos.dir)%4) == 3) {
+		if (pos.x>n-1) {
+			pos.x = pos.x-n;
+		}
+		else {
+			pos.x = 0;
+		}
 	}
 }
 
@@ -243,11 +219,17 @@ function traLef(n) {
 	if(pos.x>n-1) {
 		pos.x = pos.x-n;
 	}
+	else {
+		pos.x = 0;
+	}
 }
 
 function traTop(n) {
 	if(pos.y>n-1) {
 		pos.y = pos.y-n;
+	}
+	else {
+		pos.y = 0;
 	}
 }
 
@@ -255,11 +237,17 @@ function traRig(n) {
 	if(pos.x<10-n) {
 		pos.x = pos.x+n;
 	}
+	else {
+		pos.x = 9;
+	}
 }
 
 function traBot(n) {
 	if(pos.y<10-n) {
 		pos.y = pos.y+n;
+	}
+	else {
+		pos.y = 9;
 	}
 }
 
@@ -313,6 +301,7 @@ function movBot(n) {
 	go(n);
 }
 
+//执行动画的坐标
 function animate () {
 	var diamondsNode = document.querySelector(".diamonds");
 	diamondsNode.style.left = pos.x*45;
@@ -320,6 +309,7 @@ function animate () {
 	diamondsNode.style.transform = "rotate("+pos.dir*90+"deg)";
 }
 
+//定位方向辅助函数
 function positive(m) {
 	var x = m;
 	while (x<0) {
